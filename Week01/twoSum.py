56cc87fa8c036e371d5182369485f6fb668e78e0
mqray <Slawenceray@163.com>
2020-06-10 00:27:07 +0800

    提交两数之和的两次hash表解法

--- Week01/twoSum.py
+++ Week01/twoSum.py
@@ -1,6 +1,3 @@
-# -*- coding:utf-8 -*-
-# Author : Ray
-# Data : 2020/6/10 12:08 AM
 # 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 #
 #  你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -29,10 +26,17 @@ class Solution(object):
         # 1.暴力解法：两重循环
         # 2.hash表记录 两次hash
         # 3.一次hash表
-        # 击败18 %
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
+        # 击败18%
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # 击败53%
+        dict = {}
+        for ind, num in enumerate(nums):  # 创建hash表
+            dict[num] = ind
+        for ind, num in enumerate(nums):  # 遍历hash表
+            j = dict.get(target - num)
+            if j and ind != j:  # 排除[2,3] 4这种情况
+                return [ind, j]
 # leetcode submit region end(Prohibit modification and deletion)
