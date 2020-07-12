a752f29c49fe27fe1e144477094a47fbfd229a0e
mqray <Slawenceray@163.com>
2020-07-12 11:31:25 +0800

    修改二叉树最近父节点

--- Week03/二叉树的最近公共祖先.py
+++ Week03/二叉树的最近公共祖先.py
@@ -12,47 +12,26 @@
 class Solution:
     #1.递归 分析出找到最近公共祖先节点的情形，如果p，q由同一祖先节点 则p、q要么位于某棵树的左右子树
     #要么在同一棵树上，在同一棵树上又有 p或q为根 另外存在p或q在其左右子树中
-    res = None
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.dfs(root,p,q)
-        return self.res
+    def lowestCommonAncestor(self, root, p, q):
+        # dfs
+        # if not root or p == root or q == root : return root
+        # left = self.lowestCommonAncestor(root.left,p,q)#表示左子树是否包含p或q
+        # right = self.lowestCommonAncestor(root.right,p,q)
+        # if left and right: return root
+        # return left if left else right
+        # hash表解法
+        hashmap = {root: None}
 
-    def dfs(self,root,p,q):
-        # recursive terminator
-        if root is None: return False
-        #current process
-        #drill down
-        root_left = self.dfs(root.left,q,p)#判断这颗树的子树中是否含有q节点或者q节点
-        root_right = self.dfs(root.right,p,q)
-        #要判断p、q的最近公共祖先，我们从根节点开始寻找，查探root的左子树和右子树是否包含q节点或p节点，此时最近公共最先就是root；另外的，如果p或q为子树根节点，且p或q处在这棵子树下面，则最进公共祖先就是p或q节点
-        if root_left and root_right or ((root.val==p.val or root.val==q.val) and(root_left or root_right) ):#对于5，1这种情况，这是后就要返回p为true给上层递归
-            self.res = root#那么就返回当前树的根节点
-        #reverse states
-        return root_left or root_right or(root.val==p.val or root.val==q.val)
+        def dfs(root):
+            if root:
+                if root.left: hashmap[root.left] = root
+                if root.right: hashmap[root.right] = root
+                dfs(root.left)
+                dfs(root.right)
 
-#2.记录父节点
-class SolutionI:
-    # ，然后我们就可以利用节点的父节点信息从p结点开始不断往上跳，并记录已经访问过的节点，再从q节点开始不断往上跳，如果碰到已经访问过的节点，那么这个节点就是我们要找的最近公共祖先。
-    all_parents = {}
-    p_parents = {}
-
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.all_parents[root.val] = None
-        self.dfs(root)  # 用哈希表存储所有节点的父节点
-        # 在all_parents找p的根节点，并且记录找到的父节点
-        while p != None:
-            self.p_parents[p.val] = True
-            p = self.all_parents.get(p.val)
-        while q != None:  # 因为p、q是必有公共父节点的，所以一定能在p_parents里面找到父节点
-            if self.p_parents.get(q.val):  # 如果如果在p的parent中找到
-                return q
-            q = self.all_parents.get(q.val)  # 如果没找到，说明q可能还要向上一层才能找到
-        return None
-
-    def dfs(self, root):  # 从root开始遍历子节点 记录子节点值作为key，值为root节点
-        if root.left is not None:
-            self.all_parents[root.left.val] = root
-            self.dfs(root.left)
-        if root.right is not None:
-            self.all_parents[root.right.val] = root
-        self.dfs(root.right)
+        dfs(root)
+        left, right = p, q  # 转化为找两个链表的相交节点
+        while left != right:
+            left = hashmap.get(left, q)
+            right = hashmap.get(right, p)
+        return left
