2c269bdb4d9802b342ea1dfddcc383775342b464
mqray <Slawenceray@163.com>
2020-07-25 00:32:45 +0800

    笔记

--- Week07/Trie树.py
+++ Week07/Trie树.py
@@ -3,64 +3,37 @@
 # Data : 2020/7/20 8:20 PM
 
 from collections import defaultdict
-# class TrieNode:
-#     def __init__(self):
-#         self.children = defaultdict(TrieNode)
-#         self.is_word = False
-#
-# class Trie:
-#     def __init__(self):
-#         self.root = TrieNode()
-#
-#     def insert(self, word: str) -> None:#Inserts a word into the trie.
-#         node = self.root
-#         for ch in word:#用word构建Trie树
-#             node = node.children[ch]
-#         node.is_word = True
-#
-#     def search(self, word: str) -> bool:#Returns if the word is in the trie.
-#         node = self.root
-#         for ch in word:
-#             if ch in node.children:
-#                 node = node.children[ch]
-#             else: return False
-#         return node.is_word
-#
-#     def startsWith(self, prefix: str) -> bool:
-#         # Returns if there is any word in the trie that starts with the given prefix.
-#         node = self.root
-#         for ch in prefix:
-#             if ch in node.children:
-#                 node = node.children[ch]
-#             else: return False
-#         return True
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
 
 class Trie:
     def __init__(self):
-        self.root = {}
-        self.end_of_word = '#'
+        self.root = TrieNode()
 
-    def insert(self,word):
+    def insert(self, word):
         node = self.root
         for ch in word:
-            if ch in node:
-                node = node.setdefault(ch,{})
-        node[self.end_of_word] = self.end_of_word
+            node = node.children[ch]
+        node.is_word = True
 
-    def search(self,word):
+    def search(self, word):
         node = self.root
         for ch in word:
-            if ch in node:
-                node = node[ch]
-            else: return False
-        return self.end_of_word in node
+            if ch in node.children:
+                node = node.children[ch]
+            else:
+                return False
+        return node.is_word
 
-    def startwiths(self,prefix):
+    def startsWith(self, prefix):
         node = self.root
         for ch in prefix:
-            if ch in node:
-                node = node[ch]
-            else: return False
+            if ch in node.children:
+                node = node.children[ch]
+            else:
+                return False
         return True
 
 
